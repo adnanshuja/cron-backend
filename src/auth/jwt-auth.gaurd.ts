97ae9c62585651b62dc590671734015666cd85c1
Muhammad Adnan Shuja <adnanshuja3@gmail.com>
2022-05-21 13:46:49 +0500

    updated gaurd

--- src/auth/jwt-auth.gaurd.ts
+++ src/auth/jwt-auth.gaurd.ts
@@ -3,10 +3,4 @@ import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtAuthGaurd extends AuthGuard('jwt') {
-     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const { user } = context.switchToHttp().getRequest();
-        console.log('testing', user);
-        return true;
-    }
-}
+export class JwtAuthGaurd extends AuthGuard('jwt') {}
